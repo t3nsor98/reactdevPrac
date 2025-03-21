fa2d27d14e88007429e9b210b3edd6b9750906ed
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-03-21 22:42:24 +0530

    added 2nd project

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
   return (
-    <>
-      <h1 className="text-4xl font-bold text-center mt-4 mb-4 ">
-        Password Generator
-      </h1>
-      <h2 className="text-3xl font-bold text-center bg-purple-500 p-4 ">
-        Teaching React Hooks
-      </h2>
-    </>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-600">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <h2 className="text-center my-4">useCallback, useRef & useEffect</h2>
+      <div className="flex shadow round-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+      </div>
+    </div>
   );
 }
 
