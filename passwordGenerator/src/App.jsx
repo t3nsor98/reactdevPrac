039c2953ffaa637c142d1fcd12577df701d777f7
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-03-21 21:33:00 +0530

    add initial setup for React app with Vite and Tailwind CSS

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-4 mb-4 ">
+        Password Generator
+      </h1>
+      <h2 className="text-3xl font-bold text-center bg-purple-500 p-4 ">
+        Teaching React Hooks
+      </h2>
+    </>
+  );
+}
+
+export default App;
