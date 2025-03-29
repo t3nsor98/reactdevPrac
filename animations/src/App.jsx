ee45583abd6ff080b0c3b356a183c336659d03c0
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-03-29 19:15:59 +0530

    feat: add animated squares component and enhance App layout with new styles

--- animations/src/App.jsx
+++ animations/src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
 import ScrollVelocity from "./components/ScrollVelocity";
+import SplashCursor from "./components/SplashCursor";
+import Squares from "./components/Squares";
+
+{
+  /* <Squares
+        speed={0.5}
+        squareSize={40}
+        direction="diagonal" // up, down, left, right, diagonal
+        borderColor="#fff"
+        hoverFillColor="#222"
+      /> */
+}
 
 function App() {
+  const velocity = 100;
   return (
     <>
+      <SplashCursor />
       <ScrollVelocity
-        texts={["React Bits", "Scroll Down"]}
+        texts={["Digbijaya Lenka", "Full Stack Developer"]}
         velocity={velocity}
         className="custom-scroll-text"
       />
